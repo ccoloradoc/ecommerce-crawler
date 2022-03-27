dacab4fe29a5e972b1dbf729941001b6bfc99c61
Cristian Colorado <cristian.colorado@sao-mc-191105a.ad.unosquare.com>
2022-03-27 12:40:28 -0600

    Adding droplet startup

--- droplet/startup.sh
+++ droplet/startup.sh
@@ -2,3 +2,7 @@
 GW=$(ip r | grep default | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
 IP=$(curl -s https://raw.githubusercontent.com/ccoloradoc/ecommerce-crawler/master/droplet/ip)
 echo "route add -host $IP gw $GW"
+
+route add -host $IP gw $GW
+
+#sudo route del -net $IP gw $GW netmask 255.255.255.255 dev eth0
