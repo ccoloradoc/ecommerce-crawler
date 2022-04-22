8a4b6372674b6d54a47106f9a409517e48c115d4
Cristian Colorado <cristian.colorado@sao-mc-191105a.ad.unosquare.com>
2022-04-22 17:05:19 -0500

    Walmart filter

--- src/parser/walmart.js
+++ src/parser/walmart.js
@@ -1,4 +1,7 @@
 const loggerFactory = require('../log/logger')
+const urlFilterRegex = /nerf|muneca|monopoly|peluche|muggs|de-mesa/
+const brandFilterRegex = /funko|gaming/
+const site = 'https://www.walmart.com.mx'
 
 module.exports = function consumeWalmart(content) {
 	const logger = loggerFactory.getInstance()
@@ -6,38 +9,50 @@ module.exports = function consumeWalmart(content) {
 		let itemsMap = {}
 		logger.info('Parsing content...')
 		let payload = JSON.parse(content)
+		let totalElements = payload.appendix.SearchResults.content.length
 		let filtered = payload.appendix.SearchResults.content
 			.filter(item => {
-				let term = item.productSeoUrl.toLowerCase()
-				if(
-					term.includes("muneca") ||
-					term.includes("nerf") ||
-					term.includes("funko") ||
-					term.includes("disney") ||
-					term.includes("monopoly") || 
-					term.includes("juego-de-mesa") ||
-					term.includes("juegos-de-mesa") ||
-					term.includes("peluche") ||
-					term.includes("muggs")
-				) {
+				let result = brandFilterRegex.test(item.brandName.toLowerCase())
+				if(result) {
+					logger.warn('Filtering out by brandName: ' + item.skuDisplayName)
 					return false
 				} else {
 					return true
 				}
 			})
-		logger.info('Processing ' + payload.appendix.SearchResults.content.length + ' filter to ' + filtered.length)
+			.filter(item => {
+				let result = urlFilterRegex.test(item.productSeoUrl.toLowerCase())
+				if(result) {
+					logger.warn('Filtering out by term: ' + item.skuDisplayName)
+					return false
+				} else {
+					return true
+				}
+			})
+		logger.info('Processing ' + totalElements + ' filter to ' + filtered.length)
 		filtered
 			.forEach((item, i) => {
 				itemsMap[item.id] = {
 					id: item.id,
 					title: item.skuDisplayName,
 					price: item.skuPrice,
-					image: 'https://www.walmart.com.mx' + item.imageUrls.large,
-					link: 'https://www.walmart.com.mx' + item.productSeoUrl.replace(/&.*=.*/, "").replace(/#.*=.*/, ""),
+					image: site + item.imageUrls.large,
+					link: site + item.productSeoUrl.replace(/&.*=.*/, "").replace(/#.*=.*/, ""),
 					store: 'Walmart'
 				}
+				if(item.hasOwnProperty('variants')) {
+					item.variants.forEach((variant, i) => {
+						itemsMap[variant.id] = {
+							id: variant.id,
+							title: variant.displayText,
+							price: variant.price,
+							image: site + variant.smallImage,
+							link: site + variant.url.replace(/&.*=.*/, "").replace(/#.*=.*/, ""),
+							store: 'Walmart'
+						}
+					});
+				}
 			});
-		
 		logger.info('Found ' + Object.entries(itemsMap).length + ' elements')
 		resolve(itemsMap)
 	});
