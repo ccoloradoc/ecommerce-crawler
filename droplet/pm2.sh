85786a3fb67501c7f669d2d90bd5681e8d3d6aae
Cristian Colorado <cristian.colorado@sao-mc-191105a.ad.unosquare.com>
2022-04-25 01:53:52 -0500

    Liverpulga

--- droplet/pm2.sh
+++ droplet/pm2.sh
@@ -2,3 +2,4 @@ pm2 start index.js --name="MLML" -- MercadolibreMarvelLegends
 pm2 start index.js --name="MLSW" -- MercadolibreStarWars
 pm2 start walmart.js --name="WMML" -- WalmartMarvelLegends
 pm2 start monitor.js --name="WMMonitor" -- MonitorWalmartItem
+pm2 start index.js --name="PulgaML" -- LiverpoolMarvelLegends
