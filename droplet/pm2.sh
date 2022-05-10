d2051bb467888227eba1baaad84deaec0819c3e8
Cristian Colorado <cristian.colorado@sao-mc-191105a.ad.unosquare.com>
2022-05-10 13:05:20 -0500

    Update pm2.sh

--- droplet/pm2.sh
+++ droplet/pm2.sh
@@ -1,5 +1,6 @@
 pm2 start index.js --name="MLML" -- MercadolibreMarvelLegends
 pm2 start index.js --name="MLSW" -- MercadolibreStarWars
+pm2 start index.js --name="MLJoe" -- MercadoLibreGIJoe
 pm2 start walmart.js --name="WMML" -- WalmartMarvelLegends
 pm2 start monitor.js --name="WMMonitor" -- MonitorWalmartItem
 pm2 start index.js --name="PulgaML" -- LiverpoolMarvelLegends
